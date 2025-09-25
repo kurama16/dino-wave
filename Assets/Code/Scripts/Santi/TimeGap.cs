2f76981079191bdb2714cc7f994ef03c2dbc7165
SantiLeonMoreno <santygoo86@gmail.com>
2025-09-25 19:02:24 -0300

    Agregue la brecha con su script, le agregue un script al enemigo para que detecte la brecha (#15)

---
+++ Assets/Code/Scripts/Santi/TimeGap.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class TimeGap : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float maxHealth = 50f;
+    private float currentHealth;
+
+    [SerializeField] private float floatAmplitude = 0.5f;
+    [SerializeField] private float floatFrequency = 2f;
+    private Vector3 startPos;
+
+    private bool isDead = false;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (isDead || amount <= 0) return;
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+
+        Destroy(gameObject);
+    }
+}
